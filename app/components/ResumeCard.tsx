dc1a39685b55fa8d4353ec66c31f292d729ae145
jinyuzhu <jinyuzhh@gmail.com>
2025-12-19 18:58:19 +0800

    add some basic components and style

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router"
+
+const ResumeCard = ({ resume }: { resume: Resume }) => {
+    return (
+        <Link to={`/resume/${resume.id}`}>ResumeCard</Link>
+    )
+}
+
+export default ResumeCard
